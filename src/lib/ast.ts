6c4ac5a25abc2ec232235a72f3a6f6969421c579
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-04-08 14:33:21 +0900

    Put literals on each line

--- src/lib/ast.ts
+++ src/lib/ast.ts
@@ -4,7 +4,9 @@ import { Translation } from '../interfaces';
 
 export const dts = (keys: Translation[]): string => {
   const declareType =
-    'type MessageKey =' + keys.map(key => `"${key.key}"`).join('|') + ';\n\n';
+    'type MessageKey =\n' +
+    keys.map(key => `  | "${key.key}"`).join('\n') +
+    ';\n\n';
   const origPath = join(__dirname, '../../assets/react-intl.d.ts');
   const orig = readFileSync(origPath, { encoding: 'utf-8' });
 
