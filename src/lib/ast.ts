0c06855b3e607491ccfde633d4e020ad82548077
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-04-08 14:55:36 +0900

    Add comments to disable Lints

--- src/lib/ast.ts
+++ src/lib/ast.ts
@@ -3,12 +3,16 @@ import { join } from 'path';
 import { Translation } from '../interfaces';
 
 export const dts = (keys: Translation[]): string => {
+  const disableCommentsForLint =
+    '/* eslint-disable */\n/* tslint:disable */\n\n\n';
+
   const declareType =
     'type MessageKey =' + keys.map(key => `"${key.key}"`).join('|') + ';\n\n';
   const origPath = join(__dirname, '../../assets/react-intl.d.ts');
   const orig = readFileSync(origPath, { encoding: 'utf-8' });
 
   return (
+    disableCommentsForLint +
     declareType +
     orig.replace(/^\s*id: string;/m, '            id: MessageKey;')
   );
